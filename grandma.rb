0aea8296b20423ba9672681f5bbe4bbbe637cb13
Simon Lee <simonlee0306@gmail.com>
2018-10-15 22:55:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
-    puts "NO, NOT SINCE 1938!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif str == str.upcase
     puts "NO, NOT SINCE 1938!"
   else
